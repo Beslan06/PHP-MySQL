f1f635cffb41cefa3c5afe1b00e5e4145c639fcc
Beslan06 <bor.mm2011@yandex.ru>
2022-12-05 14:06:17 +0300

    PHP-MySQL

---
+++ variables.php
@@ -0,0 +1,8 @@
+<?php
+    $_profession = 'Программист';
+    $_city_country = 'Россия, Магас';
+    $_email = 'bor.mm2011@yandex.ru';
+    $_mobile = '+7 920 666 00 00';
+
+    echo $_profession
+?>
